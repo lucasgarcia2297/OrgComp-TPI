47243ef04b832b0bd600c3173f7564c36a1eaee3
lucasgarcia2297 <lucas.garcia2297@gmail.com>
2024-10-14 11:32:12 -0300

    corregí errores en tb_BR.v y tb_SE.v

--- Testbenchs/tb_BR.v
+++ Testbenchs/tb_BR.v
@@ -7,71 +7,67 @@ module tb_BR();
     reg[31:0] wd;
     wire[31:0] rd1, rd2;  
 
-BR uut(
-    .clk(clk),
-    .a1(a1),
-    .a2(a2),
-    .a3(a3),
-    .we(we),
-    .wd(wd),
-    .rd1(rd1),
-    .rd2(rd2)
-);
+    BR uut(
+        .clk(clk),
+        .a1(a1),
+        .a2(a2),
+        .a3(a3),
+        .we(we),
+        .wd(wd),
+        .rd1(rd1),
+        .rd2(rd2)
+    );
 
- // Generador de reloj
+    // Generador de reloj
     always #5 clk = ~clk;
 
-initial begin
-    $dumpfile("./gtkwave/tb_BR.vcd");
-    $dumpvars(0, tb_BR);
+    initial begin
+        $dumpfile("./gtkwave/tb_BR.vcd");
+        $dumpvars(0, uut); 
+        clk = 0;
+        we = 0;
+        a1 = 0; 
+        a2 = 0;
+        a3 = 0;
+        wd = 0;
 
-    clk = 0;
-    we = 0;
-    a1 = 0; 
-    a2 = 0;
-    a3 = 0;
-    wd = 0;
+        // Prueba 1: Escribir y leer en la dirección 1
+        #10;
+        we = 1;
+        a3 = 5'b00001;
+        wd = 32'hAAAA_AAAA;
+        #10;
+        we = 0;
+        a1 = 5'b00001;
+        #10;
 
-    //Prueba 1: Escribir y leer en la dirección 1
-    #10;
-    we = 1;
-    a3 = 5'b00001;
-    wd = 32'hAAAA_AAAA;
-    #10;
-    we = 0;
-    a1 = 5'b00001;
-    #10;
+        // Prueba 2: Escribir y leer en la dirección 2
+        we = 1;
+        a3 = 5'b00010;
+        wd = 32'hBBBB_BBBB;
+        #10;
+        we = 0;
+        a2 = 5'b00010;
+        #10;
 
-    // Prueba 2: Escribir y leer en la dirección 2
-    we = 1;
-    a3 = 5'b00010;
-    wd = 32'hBBBB_BBBB;
-    #10;
-    we = 0;
-    a2 = 5'b00010;
-    #10;
+        // Prueba 3: Escribir y leer en la dirección 3
+        we = 1;
+        a3 = 5'b00011;
+        wd = 32'hCCCC_CCCC;
+        #10;
+        we = 0;
+        a1 = 5'b00011;
+        #10;
 
-        
-    // Prueba 3: Escribir y leer en la dirección 3
-    we = 1;
-    a3 = 5'b00011;
-    wd = 32'hCCCC_CCCC;
-    #10;
-    we = 0;
-    a1 = 5'b00011;
-    #10;
+        // Prueba 4: Escribir y leer en la dirección 4
+        we = 1;
+        a3 = 5'b00100;
+        wd = 32'hDDDD_DDDD;
+        #10;
+        we = 0;
+        a2 = 5'b00100;
+        #10;
 
-    // Prueba 4: Escribir y leer en la dirección 4
-    we = 1;
-    a3 = 5'b00100;
-    wd = 32'hDDDD_DDDD;
-    #10;
-    we = 0;
-    a2 = 5'b00100;
-    #10;
-
-
-end;
-
-
-endmodule;
+        $finish;
+    end
+endmodule
--- Testbenchs/tb_SE.v
+++ Testbenchs/tb_SE.v
@@ -15,43 +15,6 @@ initial begin
     $dumpfile("./gtkwave/tb_SE.vcd");
     $dumpvars(0, tb_SE);
     
-    // // TEST 1: Instrucción tipo I (src = 00)
-    // // inm[24:13] = 12'b111111111111 (bit de signo inm[24] es 1)
-    // // Debe extender el bit de signo y tomar los bits [24:13] del inmediato.
-    // inm = 25'b1111111111110000000000000;
-    // src = 2'b00;
-    // #10;
-    // // inmExt esperado: 0xFFFFFFFF (extensión del bit de signo)
-        
-    // // TEST 2: Instrucción tipo S (src = 01)
-    // // inm[24:18] = 7'b0000001, inm[4:0] = 5'b11111
-    // // inmExt esperado: {{20{inm[24]}}, inm[24:18], inm[4:0]} = 0xFF00003F (extensión del bit de signo con combinación de bits)
-    // inm = 25'b0000001111110000001111111;
-    // src = 2'b01;
-    // #10;
-    // // inmExt esperado: 0xFF00003F
-    
-    // // TEST 3: Instrucción tipo B (src = 10)
-    // // inm[24] = 1, inm[0] = 0, inm[23:18] = 6'b101010, inm[4:1] = 4'b1100
-    // inm = 25'b1101010110000000000111100;
-    // src = 2'b10;
-    // #10;
-    // // inmExt esperado: {{19{inm[24]}}, inm[24], inm[0], inm[23:18], inm[4:1], 1'b0} = 0xFF500C00
-    
-    // // TEST 4: Instrucción tipo I con signo 0
-    // // inm[24:13] = 12'b000000000001
-    // inm = 25'b0000000000010000000000000;
-    // src = 2'b00;
-    // #10;
-    // // inmExt esperado: 0x00001000 (extensión del bit de signo en 0)
-    
-    // // TEST 5: Instrucción tipo S con bits de menor valor
-    // // inm[24:18] = 7'b0000010, inm[4:0] = 5'b00010
-    // inm = 25'b0000010000100000000000010;
-    // src = 2'b01;
-    // #10;
-    // // inmExt= 0x00000202
-    
     //TEST TIPO I
     inm = 25'b0000000000110000000001000; //addi s0, zero, 3
     src = 2'b00; 
@@ -73,8 +36,7 @@ initial begin
     src = 2'b11; 
     // inmExt = 11111001110100111000; //Este debe ser el resultado.
     #10; 
-        
-        
+    
 end;
 endmodule;
 
